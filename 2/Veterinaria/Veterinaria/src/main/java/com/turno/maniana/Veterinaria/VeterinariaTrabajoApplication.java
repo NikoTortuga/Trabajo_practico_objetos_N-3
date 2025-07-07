7f23986713457ca69a493d28ddb9cfa86d826e0d
NikoTortuga <nikoletortuga0@gmail.com>
2025-07-07 03:17:54 -0300

    Add files via upload

---
+++ 2/Veterinaria/Veterinaria/src/main/java/com/turno/maniana/Veterinaria/VeterinariaTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.turno.maniana.Veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaTrabajoApplication.class, args);
+	}
+
+}
